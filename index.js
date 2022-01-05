7355f0d9f357d3d168856d807b6bb43b0f24f35c
Diego Teixeira <diegogteixeira@gmail.com>
2022-01-05 11:40:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+/*
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("I got clicked!");
+}
+*/
+
+
+//Detecting button press
+
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+
+
+//Detecting keyboard press
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+})
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "j":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "k":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(key);
+  }
+
+}
